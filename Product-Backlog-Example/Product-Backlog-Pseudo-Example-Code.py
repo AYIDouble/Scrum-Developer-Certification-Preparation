4de8ad99cffc55c75225da8455566998d19f2595
AYIDouble <alpaypython@gmail.com>
2018-12-16 14:38:10 +0100

    ➕ Add Example Code

---
+++ Product-Backlog-Example/Product-Backlog-Pseudo-Example-Code.py
@@ -0,0 +1,14 @@
+# NOTE : The product owner is in charge of the product backlog
+# The product owner rates the importance of each item to the organization ranging from very high to very low or another method.
+
+# Has Priority 1, lowest Priority
+def resetPassword(password):
+	print(password)
+
+# Has Priority 10, highest Priority
+def configureViewData(ViewData):
+	print(ViewData)
+
+# The Method with the highest Priority should be implemented first
+# The work that the team commits to tackle in a Sprint is the Sprint backlog, and this is separate from the product backlog. 
+# The Sprint backlog does not change during the sprint. Items are considered complete and removed from both the Sprint and product backlogs when they are “done done.”
